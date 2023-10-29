b1d582f414bf750b169031821a190965b9a267e4
Denis <denok629@gmail.com>
2023-10-29 11:43:27 +0300

    fix hw3

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -4,51 +4,149 @@ import sys
 from custom_list import CustomList
 
 
+def are_lists_eq(list1, list2):
+    if type(list1) != type(list2):
+        return False
+    if len(list1) != len(list2):
+        return False
+    for i in range(len(list1)):
+        if list1[i] != list2[i]:
+            return False
+    return True
+
+
 class TestMySort(unittest.TestCase):
-    def test_add_customlists(self):
+    def test_add_customlists_2_greater_1(self):
         list1 = CustomList([1, 2, 3])
         list2 = CustomList([3, 4, 5, 6])
-        self.assertListEqual(list1 + list2, CustomList([4, 6, 8, 6]))
+        res = list1 + list2
+        self.assertTrue(are_lists_eq(res, CustomList([4, 6, 8, 6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([3, 4, 5, 6])))
+
+    def test_add_customlists_1_greater_2(self):
+        list1 = CustomList([3, 4, 5, 6])
+        list2 = CustomList([1, 2, 3])
+        res = list1 + list2
+        self.assertTrue(are_lists_eq(res, CustomList([4, 6, 8, 6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([3, 4, 5, 6])))
+        self.assertTrue(are_lists_eq(list2, CustomList([1, 2, 3])))
 
     def test_add_customlist_with_list(self):
         list1 = CustomList([1, 2, 3])
-        self.assertListEqual(list1 + [3, 4, 5, 6], CustomList([4, 6, 8, 6]))
+        list2 = [3, 4, 5, 6]
+        res = list1 + list2
+        self.assertTrue(are_lists_eq(res, CustomList([4, 6, 8, 6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, [3, 4, 5, 6]))
 
     def test_add_list_with_customlist(self):
         list1 = CustomList([1, 2, 3])
-        self.assertListEqual([3, 4, 5, 6] + list1, CustomList([4, 6, 8, 6]))
+        list2 = [3, 4, 5, 6]
+        res = list2 + list1
+        self.assertTrue(are_lists_eq(res, CustomList([4, 6, 8, 6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, [3, 4, 5, 6]))
 
-    def test_add_with_empty(self):
+    def test_add_with_empty_right(self):
         list1 = CustomList([1, 2, 3])
-        self.assertListEqual([] + list1, CustomList([1, 2, 3]))
+        list2=[]
+        res = list2 + list1
+        self.assertTrue(are_lists_eq(res, CustomList([1,2,3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, []))
+        list2 = CustomList([])
+        res = list2 + list1
+        self.assertTrue(are_lists_eq(res, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([])))
+
+    def test_add_with_empty_left(self):
+        list1 = CustomList([1, 2, 3])
+        list2=[]
+        res = list1 + list2
+        self.assertTrue(are_lists_eq(res, CustomList([1,2,3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, []))
+        list2 = CustomList([])
+        res = list1 + list2
+        self.assertTrue(are_lists_eq(res, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([])))
 
     def test_add_empty(self):
-        self.assertListEqual(CustomList([]) + CustomList([]), CustomList([]))
+        list1 = CustomList([])
+        list2 = CustomList([])
+        res = list1 + list2
+        self.assertTrue(are_lists_eq(res, CustomList([])))
+        self.assertTrue(are_lists_eq(list1, CustomList([])))
+        self.assertTrue(are_lists_eq(list2, CustomList([])))
 
     def test_sub_customlists(self):
         list1 = CustomList([1, 2, 3])
         list2 = CustomList([3, 4, 5, 6])
-        self.assertListEqual(list1 - list2, CustomList([-2, -2, -2, -6]))
+        res = list1 - list2
+        self.assertTrue(are_lists_eq(res, CustomList([-2, -2, -2, -6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([3, 4, 5, 6])))
 
     def test_sub_customlists_invert(self):
         list1 = CustomList([1, 2, 3])
         list2 = CustomList([3, 4, 5, 6])
-        self.assertListEqual(list2 - list1, CustomList([2, 2, 2, 6]))
+        res = list2 - list1
+        self.assertTrue(are_lists_eq(res, CustomList([2, 2, 2, 6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([3, 4, 5, 6])))
 
     def test_sub_customlist_with_list(self):
         list1 = CustomList([1, 2, 3])
-        self.assertListEqual(list1 - [3, 4, 5, 6], CustomList([-2, -2, -2, -6]))
+        list2 = [3, 4, 5, 6]
+        res = list1 - list2
+        self.assertTrue(are_lists_eq(res, CustomList([-2, -2, -2, -6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, [3, 4, 5, 6]))
 
     def test_sub_list_with_customlist(self):
         list1 = CustomList([1, 2, 3])
-        self.assertListEqual([3, 4, 5, 6] - list1, CustomList([2, 2, 2, 6]))
+        list2 = [3, 4, 5, 6]
+        res = list2 - list1
+        self.assertTrue(are_lists_eq(res, CustomList([2, 2, 2, 6])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, [3, 4, 5, 6]))
 
-    def test_sub_with_empty(self):
+    def test_sub_with_empty_left(self):
         list1 = CustomList([1, 2, 3])
-        self.assertListEqual([] - list1, CustomList([-1, -2, -3]))
+        list2 = []
+        res = list2 - list1
+        self.assertTrue(are_lists_eq(res, CustomList([-1,-2,-3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, []))
+        list2 = CustomList([])
+        res = list2 - list1
+        self.assertTrue(are_lists_eq(res, CustomList([-1, -2, -3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([])))
+
+    def test_sub_with_empty_right(self):
+        list1 = CustomList([1, 2, 3])
+        list2 = []
+        res = list1 - list2
+        self.assertTrue(are_lists_eq(res, CustomList([1,2,3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, []))
+        list2 = CustomList([])
+        res = list1 - list2
+        self.assertTrue(are_lists_eq(res, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list1, CustomList([1, 2, 3])))
+        self.assertTrue(are_lists_eq(list2, CustomList([])))
 
     def test_sub_empty(self):
-        self.assertListEqual(CustomList([]) - CustomList([]), CustomList([]))
+        list1 = CustomList([])
+        list2 = CustomList([])
+        res = list1 - list2
+        self.assertTrue(are_lists_eq(res, CustomList([])))
+        self.assertTrue(are_lists_eq(list1, CustomList([])))
+        self.assertTrue(are_lists_eq(list2, CustomList([])))
 
     def test_equal_true(self):
         list1 = CustomList([1, 2, 3])
@@ -112,12 +210,10 @@ class TestMySort(unittest.TestCase):
 
     def test_neg(self):
         list1 = CustomList([2, -1, 0])
-        self.assertListEqual(-list1, CustomList([-2, 1, 0]))
+        res=-list1
+        self.assertTrue(are_lists_eq(res, CustomList([-2,1,0])))
+        self.assertTrue(are_lists_eq(list1, CustomList([2, -1, 0])))
 
     def test___str__(self):
         list1 = CustomList([2, -1, 0])
-        captured_output = StringIO()
-        sys.stdout = captured_output
-        print(list1)
-        self.assertEqual(captured_output.getvalue(),
-                         "List's elements: 2 -1 0 \nSum: 1\n")
+        self.assertEqual(str(list1), "List's elements: 2 -1 0 \nSum: 1")
