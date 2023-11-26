e7955385dd4e063ab3094b48fe13029b1f03c9f1
Denis <denok629@gmail.com>
2023-11-26 15:59:22 +0300

    fix hw5

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -11,21 +11,21 @@ class TestLRUCache(unittest.TestCase):
         for test_val in range(limit):
             self.assertEqual(test_cache.get(test_val), test_val)
 
-        self.assertEqual(test_cache.get(limit + 1), None)
-        test_cache.set(limit + 1, limit + 1)
-        self.assertEqual(test_cache.get(limit + 1), limit + 1)
+        self.assertEqual(test_cache.get(5), None)
+        test_cache.set(5, 5)
+        self.assertEqual(test_cache.get(5), 5)
         self.assertEqual(test_cache.get(0), None)
         self.assertEqual(test_cache.limit, limit)
 
     def test_limit_1(self):
         limit = 1
         test_cache = LRUCache(limit)
-        for test_val in range(limit + 3):
+        for test_val in range(4):
             test_cache.set(test_val, test_val)
-        for test_val in range(limit + 2):
+        for test_val in range(3):
             self.assertEqual(test_cache.get(test_val), None)
 
-        self.assertEqual(test_cache.get(limit + 2), limit + 2)
+        self.assertEqual(test_cache.get(3), 3)
 
     def test_limit_2(self):
         limit = 2
@@ -35,10 +35,10 @@ class TestLRUCache(unittest.TestCase):
         for test_val in range(limit):
             self.assertEqual(test_cache.get(test_val), test_val)
 
-        self.assertEqual(test_cache.get(limit + 1), None)
+        self.assertEqual(test_cache.get(3), None)
         self.assertEqual(test_cache.get(0), 0)
-        test_cache.set(limit + 1, limit + 1)
-        self.assertEqual(test_cache.get(limit + 1), limit + 1)
+        test_cache.set(3, 3)
+        self.assertEqual(test_cache.get(3), 3)
         self.assertEqual(test_cache.get(0), 0)
         self.assertEqual(test_cache.get(1), None)
 
@@ -62,3 +62,33 @@ class TestLRUCache(unittest.TestCase):
     def test_invalid_limit(self):
         test_cache = LRUCache(-1)
         self.assertEqual(test_cache.limit, 42)
+
+    def test_like_in_task(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        self.assertEqual(cache.get("k3"),None)
+        self.assertEqual(cache.get("k2"),"val2")
+        self.assertEqual(cache.get("k1"),"val1")
+
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k3"),"val3")
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual (cache.get("k1") ,"val1")
+
+    def test_update_existing_key(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k1","val3")
+        cache.set("k3","val")
+
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual(cache.get("k3"),"val")
+        self.assertEqual (cache.get("k1") ,"val3")
+
+
